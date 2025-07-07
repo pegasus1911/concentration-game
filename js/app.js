87c2001b83e424d0a62dc31c8fb2492eba14643f
ali <alihasansaad1@gmail.com>
2025-07-07 23:54:23 +0300

    finnally its working after 4 days, everything is kind of done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,86 @@
+
 /*-------------- Constants -------------*/
-
-
+const howToPlayBtn = document.querySelector('.how-to-play');
+const instructionsElement = document.querySelector('.instructions');
+const closeBtn = document.querySelector('.close-btn');
+const cards = document.querySelectorAll('.card');
+const resetButton=document.querySelector('.reset')
 /*---------- Variables (state) ---------*/
-
-
-/*----- Cached Element References  -----*/
-
-
+let cardsHasFlipped = false;
+let firstCard, secondCard;
+let stopGame = false;
 /*-------------- Functions -------------*/
 
+(function shuffle() {
+  cards.forEach(card => {
+    card.style.order = Math.floor(Math.random() * 12);
+  });
+})();
+function flipCard() {
+  if (stopGame==true || this === firstCard){
+  return;
+  }
+
+  this.classList.add('flip');
+
+  if (cardsHasFlipped==false) {
+    cardsHasFlipped = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+function checkForMatch() {
+  let matchFound = firstCard.dataset.name === secondCard.dataset.name;
+  if(matchFound==true){
+    disableCards() 
+  }
+  else{
+    unflipCards()
+  }
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  stopGame = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 2000);
+}
+
+function resetBoard() {
+  cardsHasFlipped=false
+  stopGame=false
+  firstCard=null
+  secondCard=null
+}
 
 /*----------- Event Listeners ----------*/
+howToPlayBtn.addEventListener('click', () => {
+  instructionsElement.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', () => {
+  instructionsElement.classList.add('hidden');
+});
+
+window.addEventListener('click', (whereHeClicked) => {
+  if (whereHeClicked.target === instructionsElement) {
+    instructionsElement.classList.add('hidden');
+  }
+});
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+resetButton.addEventListener('click', () => {
+  location.reload();
+});
